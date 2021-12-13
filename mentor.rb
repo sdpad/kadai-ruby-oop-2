c76a55e2924c13112bd2e90d00780712e905386c
sd.ka <sdpad75@gmail.com>
2021-12-13 10:32:09 +0000

    change sentence

--- mentor.rb
+++ mentor.rb
@@ -13,11 +13,11 @@ end
 
 class RailsMentor < Mentor
   def job
-    puts "#{self.name}です。私はRubyとRailsでwebアプリケーションを作ります。"
+    puts "#{self.name}です。私はRubyとRailsでWebアプリケーションを作ります。"
   end 
 end
 
-kirameki = Mentor.new(name: "煌")
+kirameki = Mentor.new(name: "煌木")
 akaide = RailsMentor.new(name: "赤出")
 
 kirameki.job
